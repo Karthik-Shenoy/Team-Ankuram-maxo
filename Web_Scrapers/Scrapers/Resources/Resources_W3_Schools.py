4b44140abf40d41f103e4e2c86c0d220663258be
Prabhu <damshallwin23@gamil.com>
2021-01-09 22:05:59 +0530

    added uploader file for w3 scools

---
+++ Web_Scrapers/Scrapers/Resources/Resources_W3_Schools.py
@@ -0,0 +1,4 @@
+Sorting,https://www.w3schools.com/howto/howto_js_sort_list.asp
+Complete-Search,https://www.w3schools.com/howto/howto_css_fullscreen_search.asp
+Greedy-Algorithms,https://www.w3schools.in/data-structures-tutorial/greedy-algorithm/
+Recursion,https://www.w3schools.com/java/java_recursion.asp
